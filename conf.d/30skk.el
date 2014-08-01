85724ae6b7cc4bb9c68d711a9af388c6bf558ff4
Safx <safxdev@gmail.com>
2014-08-01 22:10:48 +0900

    add ddskk settings

---
+++ conf.d/30skk.el
@@ -0,0 +1,15 @@
+(require 'skk-autoloads)
+(setq skk-large-jisyo "~/.emacs.d/skk-jisyo.ML")
+(setq skk-date-ad nil)
+(setq skk-number-style nil)
+(setq skk-rom-kana-rule-list
+      '(("@" nil "@")
+        ("z " nil "　")
+        ("z{" nil "【】")
+        ("z}" nil "〔〕")))
+
+;(global-set-key "\C-x\C-j" 'skk-mode)
+;(global-set-key "\C-x\C-j" 'toggle-input-method)
+(global-set-key (kbd "C-x j") 'skk-auto-fill-mode)
+
+
