4c3a092c5362a78c4049f1b4c8f8a385bed0502f
Safx <safxdev@gmail.com>
2014-08-01 22:54:58 +0900

    add helm settings

--- init.el
+++ init.el
@@ -1,12 +1,13 @@
-;; ensure init-loader is loaded 
+;; ensure package.el is ready
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+;; ensure init-loader is ready
 (when (not (package-installed-p 'init-loader))
   (package-refresh-contents)
   (package-install 'init-loader))
-
 
 (require 'init-loader)
 (init-loader-load (expand-file-name "~/.emacs.d/conf.d"))
