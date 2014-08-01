93333d58a563ee6e0e81d7f2de1ae22ab0927006
Safx <safxdev@gmail.com>
2014-08-01 22:28:38 +0900

    modify global keys

--- conf.d/10keys.el
+++ conf.d/10keys.el
@@ -3,15 +3,27 @@
 (global-set-key "\C-z" 'call-last-kbd-macro)
 (global-set-key "\C-h" 'backward-delete-char-untabify)
 (global-set-key "\M-h" 'backward-kill-word)
-(global-set-key "\M-j" 'goto-line)
+;(global-set-key "\M-j" 'goto-line)
 (global-set-key "\M-o" 'dabbrev-expand)
-(global-set-key [(super ?o)] 'ispell-complete-word)
+;(global-set-key [(super ?o)] 'ispell-complete-word)
 ;(global-set-key "\M-@" 'lisp-complete-symbol)
-(global-set-key "\C-xq" 'menu-bar-mode)
+;(global-set-key "\C-xq" 'menu-bar-mode)
+
+(global-set-key "\C-xq"
+                '(lambda ()
+                   (interactive)
+                   (setq truncate-lines (not truncate-lines))
+                   (recenter)))
+
+;; copy when read only mode
+(global-set-key "\C-k"
+                '(lambda ()
+                   (interactive)
+                   (if (not buffer-read-only)
+                       (kill-line)
+                     (kill-new (buffer-substring (point) (line-end-position))))))
+
+;; define-keys
 (define-key esc-map "&" 'query-replace-regexp)
 (define-key global-map "\C-x\C-h" 'help-command)
-(global-set-key "\C-xq" '(lambda ()
-                           (interactive)
-                           (setq truncate-lines (not truncate-lines)
-                                 (recenter nil))))
-(define-key minibuffer-local-completion-map "\C-w" 'backward-kill-word) ; ミニバッファ内C-wで単語削除
+(define-key minibuffer-local-completion-map "\C-w" 'backward-kill-word)
