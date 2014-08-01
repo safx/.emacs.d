b051b20f4a1872b77451458364776410e5ebd513
Safx <safxdev@gmail.com>
2014-08-01 23:26:22 +0900

    ff-find-other-file

--- conf.d/50cpp.el
+++ conf.d/50cpp.el
@@ -1,7 +1,7 @@
 (add-hook 'c-mode-common-hook
           '(lambda()
              (c-set-style "cc-mode")
-             (local-set-key "\M-j" 'goto-line)
+             (local-set-key  (kbd "C-c C-h") 'ff-find-other-file)
              (setq-default indent-tabs-mode nil)
              (setq c-basic-offset 4)
              (setq-default tab-width 4)))
