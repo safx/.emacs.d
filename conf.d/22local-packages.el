43e1f73d32dcd6dd7b662cd7f5cb70955ac768e5
Safx <safxdev@gmail.com>
2014-08-01 22:18:51 +0900

    add some libralies

---
+++ conf.d/22local-packages.el
@@ -0,0 +1,15 @@
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+
+
+(require 'redo)
+(global-set-key "\C-\\" 'redo)
+
+
+(require 'yank-pop-summary)
+(global-set-key "\M-y" 'yank-pop-forward)
+(global-set-key "\C-\M-y" 'yank-pop-backward)
+
+
+(require 'dabbrev-highlight)
+(set-face-background dabbrev-highlight-face "#0000c0")
