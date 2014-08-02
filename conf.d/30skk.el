9fa6a430c2ff7a1ffb849b762fbbd7971a79687d
Safx <safxdev@gmail.com>
2014-08-02 22:39:55 +0900

    set global-key C-j to SKK-mode

--- conf.d/30skk.el
+++ conf.d/30skk.el
@@ -1,5 +1,5 @@
 (require 'skk-autoloads)
-(setq skk-large-jisyo "~/.emacs.d/skk-jisyo.ML")
+;(setq skk-large-jisyo "~/.emacs.d/skk-jisyo.ML")
 (setq skk-date-ad nil)
 (setq skk-number-style nil)
 (setq skk-rom-kana-rule-list
@@ -8,8 +8,10 @@
         ("z{" nil "【】")
         ("z}" nil "〔〕")))
 
-;(global-set-key "\C-x\C-j" 'skk-mode)
+
 ;(global-set-key "\C-x\C-j" 'toggle-input-method)
+(global-set-key "\C-x\C-j" 'skk-mode)
 (global-set-key (kbd "C-x j") 'skk-auto-fill-mode)
+
 
 
