24c2192962177f5bb25347d6211be98bc46ef7be
Safx <safxdev@gmail.com>
2014-08-04 21:53:15 +0900

    add some global-set-keys

--- conf.d/40helm.el
+++ conf.d/40helm.el
@@ -1,3 +1,3 @@
-(global-set-key (kbd "C-:") 'helm-ls-git-ls)
-(global-set-key (kbd "C-x C-r") 'helm-recentf)
+(global-set-key (kbd "C-x C-g") 'helm-ls-git-ls)
+(global-set-key (kbd "C-x C-r") 'helm-for-files)
 
