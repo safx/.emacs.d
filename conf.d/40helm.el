6795e81abead09f59604e01b7e51594d0ea8661e
Safx <safxdev@gmail.com>
2014-08-07 21:43:35 +0900

    modify tab and C-h settings in helm

--- conf.d/40helm.el
+++ conf.d/40helm.el
@@ -1,3 +1,11 @@
 (global-set-key (kbd "C-x C-g") 'helm-ls-git-ls)
 (global-set-key (kbd "C-x C-r") 'helm-for-files)
 
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-generic-files-map (kbd "C-h") 'delete-backward-char)
+
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-generic-files-map (kbd "TAB") 'helm-execute-persistent-action)
