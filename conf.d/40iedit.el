008ffac95d025e9ced4fa8cc936348bd4ee0cb96
Safx <safxdev@gmail.com>
2014-08-07 21:37:37 +0900

    use define-key rather than local-set-key

--- conf.d/40iedit.el
+++ conf.d/40iedit.el
@@ -1,5 +1,5 @@
 (require 'iedit)
 (global-set-key (kbd "C-;") 'iedit-mode)
 (add-hook 'iedit-mode-hook
-          '(lambda()
-             (local-set-key "\C-h" 'backward-delete-char-untabify)))
+          (lambda ()
+            (define-key iedit-mode-keymap (kbd "C-h") 'backward-delete-char-untabify)))
