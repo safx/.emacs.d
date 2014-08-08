e43f4a3d488c3c1dd4695de0bd5038eb2a8704b6
Safx <safxdev@gmail.com>
2014-08-08 19:59:30 +0900

    add helm-M-x

--- conf.d/40helm.el
+++ conf.d/40helm.el
@@ -1,11 +1,17 @@
 (global-set-key (kbd "C-x C-g") 'helm-ls-git-ls)
 (global-set-key (kbd "C-x C-r") 'helm-for-files)
+(global-set-key (kbd "M-x") 'helm-M-x)
 
-(define-key helm-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-generic-files-map (kbd "C-h") 'delete-backward-char)
+(add-hook
+ 'helm-after-initialize-hook
+ (lambda ()
+   (define-key helm-map (kbd "C-h") 'delete-backward-char)
+   (define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
+   (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+   (define-key helm-generic-files-map (kbd "C-h") 'delete-backward-char)
 
-(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
-(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
-(define-key helm-generic-files-map (kbd "TAB") 'helm-execute-persistent-action)
+   (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+   (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+   (define-key helm-generic-files-map (kbd "TAB") 'helm-execute-persistent-action)
+   (define-key helm-M-x-map (kbd "TAB") 'helm-execute-persistent-action)
+))
