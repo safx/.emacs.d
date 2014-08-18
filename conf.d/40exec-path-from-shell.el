0293b0c4ae3549b2093305c8d177778b0a63f7b4
Safx <safxdev@gmail.com>
2014-08-18 20:50:33 +0900

    add a condition in exec-path-from-shell

--- conf.d/40exec-path-from-shell.el
+++ conf.d/40exec-path-from-shell.el
@@ -1,3 +1,4 @@
-(require 'exec-path-from-shell)
-(let ((envs '("PATH")))
-  (exec-path-from-shell-copy-envs envs))
+(unless (eq system-type 'windows-nt)
+  (require 'exec-path-from-shell)
+  (let ((envs '("PATH")))
+    (exec-path-from-shell-copy-envs envs)))
