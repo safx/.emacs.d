ec89b817c0d6a71b134f9642465d33487be44deb
Safx <safxdev@gmail.com>
2014-08-22 21:40:12 +0900

    add Obj-C setting to cc-other-file-alist for ff-find-other-file

--- conf.d/50cpp.el
+++ conf.d/50cpp.el
@@ -1,5 +1,9 @@
 (add-hook 'c-mode-common-hook
           '(lambda()
+             (require 'find-file)
+             (nconc (cadr (assoc "\\.h\\'" cc-other-file-alist)) '(".m" ".mm"))
+             (setq cc-other-file-alist (append cc-other-file-alist '(("\\.m\\'" (".h")) ("\\.mm\\'" (".h")))))
+
              (c-set-style "cc-mode")
              (local-set-key  (kbd "C-c C-h") 'ff-find-other-file)
              (setq-default indent-tabs-mode nil)
