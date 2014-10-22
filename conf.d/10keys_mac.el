c52ae9c839e8c5fd13b68cc3b2f7186c6bb79476
Safx <safxdev@gmail.com>
2014-10-22 20:27:41 +0900

    update mac keybinds for Emacs 24.4

--- conf.d/10keys_mac.el
+++ conf.d/10keys_mac.el
@@ -1,29 +1,14 @@
 (when (eq system-type 'darwin)
 
-;;; https://gist.github.com/railwaycat/3498096
-(global-set-key [(hyper a)] 'mark-whole-buffer)
-(global-set-key [(hyper v)] 'yank)
-(global-set-key [(hyper c)] 'kill-ring-save)
-(global-set-key [(hyper s)] 'save-buffer)
-(global-set-key [(hyper l)] 'goto-line)
-(global-set-key [(hyper z)] 'undo)
+  ;; https://gist.github.com/railwaycat/3498096
 
-(defun mac-switch-meta nil 
-  "switch meta between Option and Command"
-  (interactive)
-  (if (eq mac-option-modifier nil)
-      (progn
-        (setq mac-option-modifier 'meta)
-        (setq mac-command-modifier 'hyper)
-        )
-    (progn 
-      (setq mac-option-modifier nil)
-      (setq mac-command-modifier 'meta)
-      )
-    )
-  )
-(mac-switch-meta)
+  (setq mac-option-modifier 'meta)
+  (setq mac-command-modifier 'hyper)
 
+  (global-set-key (kbd "H-a") 'mark-whole-buffer)
+  (global-set-key (kbd "H-v") 'yank)
+  (global-set-key (kbd "H-c") 'kill-ring-save)
+  (global-set-key (kbd "H-s") 'save-buffer)
+  (global-set-key (kbd "H-l") 'goto-line)
+  (global-set-key (kbd "H-z") 'undo)
 )
-
-  
