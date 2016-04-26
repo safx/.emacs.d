ba490a0ffec3778824b39752dac90f6119bb8886
Safx <safxdev@gmail.com>
2016-04-26 23:53:36 +0900

    code highlight in org src

---
+++ conf.d/40org.el
@@ -0,0 +1,13 @@
+(setq org-src-fontify-natively t)
+
+(defface org-block-begin-line
+  '((t (:foreground "#900000" :background "#282828")))
+  "Face used for the line delimiting the begin of source blocks.")
+
+(defface org-block-background
+  '((t (:background "#282828")))
+  "Face used for the source block background.")
+
+(defface org-block-end-line
+  '((t (:foreground "#900000" :background "#282828")))
+  "Face used for the line delimiting the end of source blocks.")
