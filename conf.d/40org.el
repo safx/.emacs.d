ac951613e9621b13aa1f1cc0355ed15d04974eee
Safx <safxdev@gmail.com>
2019-02-20 00:40:27 +0900

    update org defface

--- conf.d/40org.el
+++ conf.d/40org.el
@@ -1,17 +1,11 @@
 (setq org-src-fontify-natively t)
 
-(defface org-block-begin-line
-  '((t (:foreground "#900000" :background "#282828")))
-  "Face used for the line delimiting the begin of source blocks.")
+(defface org-block-begin-line '((t (:foreground "#900000" :background "#2c2c22"))) "MY Customized face")
+(defface org-block-end-line   '((t (:foreground "#900000" :background "#2c2c22"))) "MY Customized face")
+(defface org-block            '((t (:background "#2c2c22"))) "MY Customized face")
 
-(defface org-block-background
-  '((t (:background "#282828")))
-  "Face used for the source block background.")
-
-(defface org-block-end-line
-  '((t (:foreground "#900000" :background "#282828")))
-  "Face used for the line delimiting the end of source blocks.")
-
-(defface org-level-2
-  '((t (:foreground "pink" :background "#282828")))
-  "Face used for the line delimiting the end of source blocks.")
+(defface org-level-1 '((t (:foreground "green" :background "#183818" :bold t :height 1.15))) "MY Customized face")
+(defface org-level-2 '((t (:foreground "green" :background "#183818" :bold t :height 1.10))) "MY Customized face")
+(defface org-level-3 '((t (:foreground "green" :background "#183818" :bold t :height 1.05))) "MY Customized face")
+(defface org-level-4 '((t (:foreground "green" :background "#183818" :bold t :height 1.00))) "MY Customized face")
+(defface org-level-5 '((t (:foreground "green" :background "#183818" :bold t :height 1.00))) "MY Customized face")
