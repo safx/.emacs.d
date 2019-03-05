cd61b7b28f3d9d49dfe24df7e98c9c2218ca831b
Safx <safxdev@gmail.com>
2019-03-05 23:49:46 +0900

    add keybind to ivy-yank-word

--- conf.d/40ivy_counsel.el
+++ conf.d/40ivy_counsel.el
@@ -7,12 +7,14 @@
 
 (setq ivy-height 30)
 
-(global-set-key (kbd "H-s") 'swiper)
-(global-set-key (kbd "H-r") 'counsel-recentf)
-(global-set-key (kbd "H-o") 'counsel-find-file)
-(global-set-key (kbd "H-p") 'counsel-M-x)
-(global-set-key (kbd "C-H-p") 'ivy-resume)
-(global-set-key (kbd "H-g") 'counsel-git-grep)
-(global-set-key (kbd "H-f") 'counsel-ag)
-(global-set-key (kbd "H-b") 'ivy-switch-buffer)
-(global-set-key (kbd "C-x C-g") 'counsel-git)
+(define-key global-map (kbd "H-s") 'swiper)
+(define-key global-map (kbd "H-r") 'counsel-recentf)
+(define-key global-map (kbd "H-o") 'counsel-find-file)
+(define-key global-map (kbd "H-p") 'counsel-M-x)
+(define-key global-map (kbd "C-H-p") 'ivy-resume)
+(define-key global-map (kbd "H-g") 'counsel-git-grep)
+(define-key global-map (kbd "H-f") 'counsel-ag)
+(define-key global-map (kbd "H-b") 'ivy-switch-buffer)
+(define-key global-map (kbd "C-x C-g") 'counsel-git)
+
+(define-key ivy-minibuffer-map (kbd "C-w") 'ivy-yank-word)
